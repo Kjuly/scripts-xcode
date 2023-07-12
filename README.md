60a7e55df7640516717091935803b0dd17bae319
Kjuly <dev@kjuly.com>
2023-07-12 14:49:36 +0800

    Add swiftlint-default.yml

--- README.md
+++ README.md
@@ -10,6 +10,21 @@ Then add the commend, e.g.:
 
     ./scripts-xcode/swiftlint.sh
 
+## swiftlint.sh
+
+- Option 1 (use the `.swiftlint.yml` that located in the same folder of *.xcodeproj):
+
+      ./scripts-xcode/swiftlint.sh
+
+- Option 2 (use the `swiftlint-default.yml` that's provided in /scripts-xcode):
+
+      ./scripts-xcode/swiftlint.sh default
+
+- Option 3 (use the custom path to your `.swiftlint.yml` file):
+   
+      ./scripts-xcode/swiftlint.sh path/to/your/custom/swiftlint_yml
+
+
 ## Troubleshoot
 
 If the script is unexecutable, just open your terminal and use the commend below to make it executable:
--- swiftlint.sh
+++ swiftlint.sh
@@ -4,7 +4,17 @@
 #
 # Usage (Add as Xcode Run Script Phase):
 #
-#   ./scripts-xcode/swiftlint.sh
+#   Option 1 (use the `.swiftlint.yml` that located in the same folder of *.xcodeproj):
+#
+#     ./scripts-xcode/swiftlint.sh
+#
+#   Option 2 (use the `swiftlint-default.yml` that's provided in /scripts-xcode):
+#
+#     ./scripts-xcode/swiftlint.sh default
+#
+#   Option 3 (use the custom path to your `.swiftlint.yml` file):
+#   
+#     ./scripts-xcode/swiftlint.sh path/to/your/custom/swiftlint_yml
 #
 
 # Skip running this scirpt if we are running previews or tests.
@@ -14,10 +24,23 @@ then
   exit 0
 fi
 
-# Run SwiftLint if it's available.
-if which swiftlint > /dev/null; then
+# Do nothing if SwiftLint is unavailable.
+if ! command -v swiftlint &> /dev/null; then
+  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+  exit 0
+fi
+
+# Get arg for the path of .swiftlint.yml file.
+if [ -z $1 ]; then
   swiftlint
 else
-  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+  path=$1
+
+  if [ $path == "default" ]; then
+    swiftlint --config "scripts-xcode/swiftlint-default.yml"
+  else
+    # The path to one or more SwiftLint configuration files, evaluated as a parent-child hierarchy.
+    swiftlint --config path
+  fi
 fi
 
